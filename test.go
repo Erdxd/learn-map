0b82a5540db145d3ddf2924721c21f64188a3c43
erdxd <tytalexsey@mail.ru>
2025-05-15 14:02:10 +1200

    update
    I delete  bug when after variant 2, 1 variant not detect map(the cycle was rebooted and was delete last changes. Now i was fix it  and now after variant 2, variant 1 will detected changes and write it in map, derive she in   terminal)

--- test.go
+++ test.go
@@ -5,27 +5,18 @@ import (
 )
 
 func main() {
-	z := ""
+	m := map[string]string{}
+menu:
 	for {
 
-		m := map[string]string{}
 		user := ""
 		fmt.Println("Что хотите сделать?")
 		fmt.Scan(&user)
 		if user == "1" {
-			for key, value := range m {
-				fmt.Println(key, value)
-
-			}
+			Printm(m)
 
 		} else if user == "2" {
-			a := ""
-			b := ""
-			fmt.Println("Название закладки")
-			fmt.Scan(&a)
-			fmt.Println("Адрес закладки")
-			fmt.Scan(&b)
-			m["a"] = b
+			m = addbookmark(m)
 
 		} else if user == "3" {
 			c := ""
@@ -34,13 +25,32 @@ func main() {
 			delete(m, c)
 
 		} else if user == "4" {
+			break menu
+		}
+
+		for key, value := range m {
+			fmt.Println(key, "=", value)
 
 		}
-		fmt.Println("Хочешь продолжить? да/нет")
-		fmt.Scan(&z)
-		if z == "нет" {
-			break
-		}
+
+	}
+
+}
+func addbookmark(m map[string]string) map[string]string {
+	var a string
+	var b string
+
+	fmt.Println("Название закладки")
+	fmt.Scan(&a)
+	fmt.Println("Адрес закладки")
+	fmt.Scan(&b)
+	m[a] = b
+	return m
+
+}
+func Printm(m map[string]string) {
+	if len(m) == 0 {
+		fmt.Println("У вас еще нету закладок")
 	}
 
 }
